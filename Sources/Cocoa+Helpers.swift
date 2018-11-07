333eb56e1696359c9a02c9bdd7009608f1818de8
Boy van Amstel <boy@boyvanamstel.nl>
2018-11-07 15:28:40 +0100

    Add macOS target

---
+++ Sources/Cocoa+Helpers.swift
@@ -0,0 +1,9 @@
+//
+//  Cocoa+Helpers.swift
+//  Cheers-macOS
+//
+//  Created by Boy van Amstel on 07/11/2018.
+//  Copyright © 2018 Hyper Interaktiv AS. All rights reserved.
+//
+
+import Foundation
